bfdd74ac1f9b73e7d5bed1317a3eaa2a637869b1
toMilanesi <tomi.milanesi@gmail.com>
2022-10-05 09:09:10 -0300

    Update recipe routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :recipes
+  resources :recipes, only: [index, create, show, destroy, new]
   resources :users 
   resources :foods, only: %i[index create show  destry new]
 
