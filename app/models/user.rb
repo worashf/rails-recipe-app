3b8d64fea5064863c6a0b5a13b8b04d11c7915e0
worashf <uppert83@gmail.com>
2022-10-05 15:11:47 +0300

    Add devise configuration for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :foods
 end
