ad4fc5ea81c0df79fa305253d08d5d73dc2348b9
toMilanesi <tomi.milanesi@gmail.com>
2022-10-05 14:56:24 -0300

    Add recipe_food new and create controller, new view

---
+++ app/controllers/recipe_foods_controller.rb
@@ -0,0 +1,27 @@
+class RecipeFoodsController < ApplicationController
+  def new
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_food = @recipe.recipe_food.new
+  end
+
+  def create
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_food = @recipe_food = @recipe.recipe_food.new(recipe_food_parametters)
+
+    if @recipe_food.save
+      respond_to do |format|
+        format.html { redirect_to recipe_path(@recipe), notice: 'Ingredient added!' }
+      end
+    else
+      respond_to do |format|
+        format.html { render :new, alert: 'Please fill all the fields!' }
+      end
+    end
+  end
+
+  private
+
+  def recipe_food_parametters
+    params.permit(:recipe_id, :food_id, :quantity)
+  end
+end
