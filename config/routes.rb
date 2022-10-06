faf101d67aa9fa594a2bb16480e600922e9772c0
worashf <uppert83@gmail.com>
2022-10-06 18:08:11 +0300

    Add shopping list routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # public recipes path
   get 'public_recipes', to: 'recipes#public'
+  get 'shopping_lists', to: 'recipe_foods#shopping_list'
   resources :recipes, only: [:index, :create, :show, :destroy, :new] do
     resources :recipe_foods, only: [:index, :create, :show, :destroy, :new, :update, :edit]
   end
