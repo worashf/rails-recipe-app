4cbbe76f3db42f82a2c365bf9a0cb1f8bebcf62b
worashf <uppert83@gmail.com>
2022-10-07 14:40:18 +0300

    Add valiation

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,5 +2,6 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_food
   has_many :food, through: :recipe_food
-  validates :name, :preparation_time, :cooking_time, presence: true
+  validates :name, :preparation_time, :cooking_time, presence: true, numericality: { only_integer: true,
+                                                                                     greater_than_or_equal_to: 0 }
 end
