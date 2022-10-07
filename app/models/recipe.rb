2906c6432aeb432fd646fdc843f70d7e1128e53b
toMilanesi <tomi.milanesi@gmail.com>
2022-10-07 13:54:09 -0300

    fix linters errors

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,5 +4,5 @@ class Recipe < ApplicationRecord
   has_many :food, through: :recipe_food
   validates :name, presence: true
   validates :preparation_time, :cooking_time, presence: true, numericality: { only_integer: true,
-    greater_than_or_equal_to: 0 }
+                                                                              greater_than_or_equal_to: 0 }
 end
