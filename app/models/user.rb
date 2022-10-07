d25dd25f7c6e33abfa5ac97d5d1337f01feb0a50
worashf <uppert83@gmail.com>
2022-10-07 20:47:33 +0300

    Add dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-  has_many :foods
+  has_many :foods, dependent: :destroy
+  has_many :recipes, dependent: :destroy
 end
